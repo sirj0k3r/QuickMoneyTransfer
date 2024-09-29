0255ee2e1b16bd1812b07d4acc13ed80bfd52726
sirj0k3r <info.ppf@sapo.pt>
2024-09-30 00:01:51 +0100

    * Updated icon's transparency;

--- QuickMoneyTransfer.lua
+++ QuickMoneyTransfer.lua
@@ -7,6 +7,12 @@ AccountBankPanel:HookScript("OnShow", function()
     if (QMT_bf.btnDepositAll == nil) then
         AddDepositAll();
     end
+    if (QMT_bf.btnWithdrawHalf == nil) then
+        AddWithdrawHalf();
+    end
+    if (QMT_bf.btnDepositHalf == nil) then
+        AddDepositHalf();
+    end
 end)
 
 function AddDepositAll()
@@ -23,8 +29,22 @@ function AddDepositAll()
     end)
 end
 
+function AddDepositHalf()
+    local frame = QMT_bf.btnDepositAll;
+    local btn = CreateFrame("Button", "btnDepositHalf", frame, "UIPanelButtonTemplate"); 
+    btn:SetSize(50, 21);
+    btn:SetText("50%"); 
+    btn:SetPoint("CENTER", frame, "RIGHT", -50, 20);
+
+    QMT_bf.btnDepositHalf = btn;
+
+    btn:SetScript("OnClick", function()
+        C_Bank.DepositMoney(2, GetMoney() / 2);
+    end)
+end
+
 function AddWithdrawAll()
-    local frame = AccountBankPanel.MoneyFrame;
+    local frame = QMT_bf;
     local btn = CreateFrame("Button", "btnWithdrawAll", frame, "UIPanelButtonTemplate"); 
     btn:SetSize(100, 21);
     btn:SetText("Withdraw All");
@@ -34,5 +54,19 @@ function AddWithdrawAll()
 
     btn:SetScript("OnClick", function()
         C_Bank.WithdrawMoney(2, C_Bank.FetchDepositedMoney(2));
+    end)
+end
+
+function AddWithdrawHalf()
+    local frame = QMT_bf.btnWithdrawAll;
+    local btn = CreateFrame("Button", "btnWithdrawHalf", frame, "UIPanelButtonTemplate"); 
+    btn:SetSize(50, 21);
+    btn:SetText("50%");
+    btn:SetPoint("CENTER", frame, "RIGHT", -50, 20);
+
+    QMT_bf.btnWithdrawHalf = btn;
+
+    btn:SetScript("OnClick", function()
+        C_Bank.WithdrawMoney(2, C_Bank.FetchDepositedMoney(2) / 2);
     end)
 end
